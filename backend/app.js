68fbdf0efca7ba449267f8c53ab17c96d0b5e3ff
Mészáros József <meszarosjozsef20020403@mailbox.unideb.hu>
2025-11-05 16:15:40 +0100

    basic API for testing

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 11000;
+const PORT = process.env.port || 11000;
+
+app.get('/', (req, res) => {
+    res.send(`Server is running on ${PORT} port`);
+});
 
 app.listen(PORT, (req, res) => {
-    res.send(`Server is running on ${PORT} port`);
-})
+    console.log(`Server is running on ${PORT} port`);
+});
