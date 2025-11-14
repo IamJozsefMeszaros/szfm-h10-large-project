f9db1ad317231ec9915da44dee1340a5dc1f46be
Mészáros József <meszarosjozsef20020403@mailbox.unideb.hu>
2025-11-14 15:29:34 +0100

    set a custom timeout for MongoDB connection

--- backend/conn/db.js
+++ backend/conn/db.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const connectDB = () => {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI, {
+        serverSelectionTimeoutMS: 10000,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
         .then(() => console.log("MongoDB connected"))
         .catch((err) => console.error("MongoDB error: ", err));
 }
